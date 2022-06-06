08e0f778424213f4dd2a4ec9dc4695857ee6ad18
cnadmin <sohamr@capitalnumbers.com>
2022-06-06 17:44:39 +0530

    timestamps in schema working now

--- models/user-joi.js
+++ models/user-joi.js
@@ -1,17 +1,18 @@
 const Mongoose = require("mongoose");
 const Joi = require("joi");
-const Joigoose = require("joigoose")(Mongoose, { convert: false , timestamps:true});
+const Joigoose = require("joigoose")(Mongoose);
 
-var joiUserSchema = Joi.object().keys({
+var joiUserSchema = Joi.object({
     name: Joi.string().required().min(3).max(10),
     email:Joi.string().email().required(),
-    phone:parseInt(Joi.string().regex(/^[6-9]\d{9}$/).required()),
-    password:Joi.string().required(),
-    timeCreated: Joi.date().timestamp()
+    phone: Joi.string().regex(/^[6-9]\d{9}$/).required(),
+    password:Joi.string().required()
 })
 
 var mongooseUserSchema = new Mongoose.Schema(
-    Joigoose.convert(joiUserSchema)
+    Joigoose.convert(joiUserSchema),{timestamps: true}
   );
 
-module.exports = Mongoose.model("User", mongooseUserSchema);
+const Users = Mongoose.model("User", mongooseUserSchema);
+
+module.exports = Users;
