d326e5ae24bf72e9b0f5ae257839f357cd64b867
rajarshi-cn <rajarshi@capitalnumbers.com>
2022-06-09 11:42:03 +0530

    consol

--- controllers/postgresController.js
+++ controllers/postgresController.js
@@ -1,97 +1,103 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require("sequelize")
 
 const sequelize = new Sequelize(process.env.POSTGRES_CONNECTION_STRING)
-const UserAuth = require('../models/user-sequelize')(sequelize);
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const register = async(req,res,next)=>{
-    await bcrypt.hash(req.body.password, 10, async(err, hashedPass)=>{
-        if(err){
-            res.json({
-                error:err
-            })
-        }
-        try{
-           const created = await UserAuth.create({
-            username: req.body.username,
-            password: hashedPass,
+const UserAuth = require("../models/user-sequelize")(sequelize)
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const register = async (req, res, next) => {
+  await bcrypt.hash(req.body.password, 10, async (err, hashedPass) => {
+    if (err) {
+      res.json({
+        error: err,
+      })
+    }
+    try {
+      const created = await UserAuth.create({
+        username: req.body.username,
+        password: hashedPass,
+        email: req.body.email,
+      })
+
+      res.status(200).send(created)
+    } catch (err) {
+      res.status(500).send(err.message)
+    }
+  })
+}
+
+const getAllUsers = async (req, res, next) => {
+  try {
+    const data = await UserAuth.findAll()
+
+    res.status(200).send(data)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+}
+
+const login = async (req, res, next) => {
+  let { username, password } = req.body
+
+  let userloggedIn = await UserAuth.findOne({ where: { username: username } })
+  console.log("userloggedIn==", userloggedIn)
+  if (userloggedIn) {
+    bcrypt.compare(password, userloggedIn.password, (err, result) => {
+      if (err) {
+        res.json({
+          error: err,
         })
-
-            res.status(200).send(created)
-        
-    }catch(err){
-            res.status(500).send(err.message)
-        }
-            
+      }
+      if (result) {
+        let token = jwt.sign({ name: userloggedIn.name }, "verySecretValue", {
+          expiresIn: "1h",
+        })
+        res.json({
+          message: "Login Successful!",
+          token,
+        })
+      } else {
+        res.json({ message: "Username and Password not matching" })
+      }
     })
+  } else {
+    res.json({
+      message: "No User found!",
+    })
+  }
 }
 
-const getAllUsers = async (req,res,next)=>{
- try{  
-       const data = await UserAuth.findAll()
-    
-        res.status(200).send(data)
-}catch(err){
+const editUser = async (req, res, next) => {
+  const id = req.params.id
+
+  if (req.body.password) {
+    await bcrypt.hash(req.body.password, 10, async (err, hashedPass) => {
+      req.body.password = hashedPass
+      try {
+        let updatedUser = await UserAuth.update(
+          req.body,
+          { where: { id: id } },
+          { new: true }
+        )
+        console.log(updatedUser)
+        res.status(200).send({ message: "Update Successful", updatedUser })
+      } catch (err) {
         res.status(500).send(err.message)
+      }
+    })
+  } else {
+    try {
+      let updatedUser = await UserAuth.update(
+        req.body,
+        { where: { id: id } },
+        { new: true }
+      )
+      res.status(200).send({ message: "Update Successful", updatedUser })
+    } catch (err) {
+      res.status(500).send({
+        message: err.message,
+      })
     }
+  }
 }
 
-const login = async(req,res,next)=>{
-    let {username, password} = req.body;
-    
-
-   let userloggedIn = await UserAuth.findOne({where:{username:username}})
-        if(userloggedIn){
-            bcrypt.compare(password, userloggedIn.password, (err,result)=>{
-                if(err){
-                    res.json({
-                        error:err
-                    })
-                }
-                if(result){
-                    let token = jwt.sign({name:userloggedIn.name},'verySecretValue',{expiresIn:'1h'})
-                    res.json({
-                        message:'Login Successful!',
-                        token
-                    
-                })
-                }else{
-                    res.json({message:'Username and Password not matching'})
-                }
-            })
-    
-        }else{
-            res.json({
-                message:'No User found!'
-            })
-        }
-    }
-
-    const editUser = async (req,res,next)=>{
-        const id = req.params.id;
-    
-        if(req.body.password){
-            await bcrypt.hash(req.body.password, 10, async (err, hashedPass)=>{
-                
-                req.body.password = hashedPass;
-                try{
-                    let updatedUser = await UserAuth.update(req.body,{where:{id:id}},{new:true});
-                    console.log(updatedUser);
-                    res.status(200).send({message: "Update Successful", updatedUser})
-                }catch(err){
-                    res.status(500).send(err.message)
-                }
-            })
-        }else{
-            try{
-        let updatedUser = await UserAuth.update(req.body,{where:{id:id}},{new:true})
-            res.status(200).send({message:"Update Successful", updatedUser})
-        }catch(err){
-            res.status(500).send({
-                message: err.message
-            })
-        }
-    }
-}
-
-module.exports = {register, getAllUsers, login, editUser}
+module.exports = { register, getAllUsers, login, editUser }
--- models/user-sequelize.js
+++ models/user-sequelize.js
@@ -24,38 +24,36 @@
 
   module.exports = user*/
 
-  const {Sequelize} = require('sequelize') 
-  
+const { Sequelize } = require("sequelize")
 
-  module.exports = (sequelize) => {
-
-    const User = sequelize.define("user", {
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        username: {
-            type: Sequelize.STRING
-        },
-        /*email: {
-            type: DataTypes.EMAIL,
-            allowNull: false
-        },
-        phone: {
-            type: DataTypes.INTEGER
-        },*/
-        password: {
-            type: Sequelize.STRING
-        },
-        
-    
-    }, {createdAt: false,
-    updatedAt: false}/*{
+module.exports = sequelize => {
+  const User = sequelize.define(
+    "user",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      // phone: {
+      //     type: DataTypes.INTEGER
+      // },
+      password: {
+        type: Sequelize.STRING,
+      },
+    },
+    { createdAt: false, updatedAt: false } /*{
         timestamps: true,
         createdAt: false, // don't add createdAt attribute
         
-    }*/)
-    return User
-
+    }*/
+  )
+  return User
 }
